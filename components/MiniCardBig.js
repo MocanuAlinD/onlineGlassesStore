5645be3efd15b086b1a9d754f315f0e99efb082a
Mocanu Alin <alin_ngt@yahoo.com>
2022-09-14 13:03:18 +0300

    Added left side with details

--- components/MiniCardBig.js
+++ components/MiniCardBig.js
@@ -6,10 +6,54 @@ import { ShoppingCart } from "@material-ui/icons";
 import Link from "next/link";
 import { FiMoreHorizontal } from "react-icons/fi";
 
+
+// style for pret
+// order them nicely
 const MiniCardBig = ({ produs, onAddToCart }) => {
   return (
-    <div className={styles.container + " d-flex flex-column mx-3 my-1"}>
-      <div className={styles.bg}></div>
+    <div className={styles.container + " d-flex flex-column m-3"}>
+      <div className={styles.bg}>
+        <div className={styles.leftSideBg}>
+          <h4 className={styles.title}>{produs.name}</h4>
+          <h5 className={styles.pret}>
+            Pret: {produs.price ? produs.price + " lei" : "-"}
+          </h5>
+          <div className={styles.stoc}>
+            <h5 className={styles.inventory}>
+              {!produs.inventory.managed
+                ? "In stoc"
+                : produs.inventory.available < 1
+                ? "Doar cu precomanda"
+                : produs.inventory.available < 4
+                ? "Stoc limitat"
+                : "In stoc"}{" "}
+            </h5>
+
+            {!produs.inventory.managed ? (
+              <div className={styles.cartContainer}>
+                <IconButton onClick={() => onAddToCart(produs.id, 1)}>
+                  <ShoppingCart className={styles.shopIcon} />
+                </IconButton>
+              </div>
+            ) : produs.inventory.available < 1 ? (
+              <div className={styles.cartContainer}>
+                <IconButton className={styles.shopIconDisabled}>
+                  <ShoppingCart />
+                </IconButton>
+              </div>
+            ) : (
+              <div className={styles.cartContainer}>
+                <IconButton
+                  onClick={() => onAddToCart(produs.id, 1)}
+                  title="Adauga in cos"
+                >
+                  <ShoppingCart className={styles.shopIcon} />
+                </IconButton>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
       <div className={styles.imageContainer}>
         <Image
           layout="fill"
